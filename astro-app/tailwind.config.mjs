0f6b71563c2632c896b6083d04e28f416f15140c
leonace924 <meobius123@gmail.com>
2025-08-04 15:41:53 -0400

    feat: Add the Tailwind css to Astro project

---
+++ astro-app/tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@sanity/astro/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+} 
